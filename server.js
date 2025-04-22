0b3a289aec428472db496dd62d1d939e4485f493
Laboratorio Computación N° 6 <claseslab6@inacap.cl>
2025-04-22 15:49:17 -0400

    actualizacion

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, () => {
-console.log('Servidor funcionando en: http://localhost:3000');
+    console.log('Servidor funcionando en: http://localhost:3000');
 });
